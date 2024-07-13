84c4756db00e678b6c9afa62c20daa6d72d76510
Daniel Junior <daniel1236100@gmail.com>
2024-07-13 12:00:34 -0300

    consumindo a api coincap

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,56 @@
-import { useState, FormEvent } from 'react'
+import { useState, FormEvent, useEffect } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
+
+interface CoinsProps{
+    id:string;
+    name:string;
+    symbol:string;
+    priceUsd:string;
+    supply:string;
+    maxSupply:string;
+    marketCapUsd:string;
+    volumeUsd24Hr:string;
+    vwap24Hr:string;
+    explorer:string;
+}
+
+interface Dataprops{
+    data:CoinsProps[]
+}
 export default function Home (){
     const [input,setInput] = useState('')
+    const [coins,setCoins] = useState<CoinsProps[]>([])
 
     const navigate = useNavigate()
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    async function getData(){
+        fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+        .then(response => response.json())
+        .then((data:Dataprops) => {
+            const coinsData = data.data
+
+            const price = Intl.NumberFormat("en-US", {
+                style:"currency",
+                currency:"USD"
+            })
+
+            const formatedResult = coinsData.map((item) =>{
+                const formated = {
+                    ...item,
+                    formatedPrice: price.format(Number(item.priceUsd))
+                }
+                return formated;
+            })
+
+            console.log(formatedResult)
+        })
+    }
 
     function handleSubmit(e: FormEvent){
         e.preventDefault();
